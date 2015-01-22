f3360c43de4d8e16721bb9bd30a4fff44419a3db
virtuecai <virtuecai@163.com>
2015-01-22 12:31:30 +0800

    首页根据分类查询展示电影列表

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,20 @@
 var Movie = require('../models/movie')
-//index page
-exports.index = function (req, res) {
+var Category = require('../models/category')
+    //index page
+exports.index = function(req, res) {
     //console.log('user in session:' + req.session.user)
-    Movie.fetch(function (err, movies) {
-        res.render('index', {
-            title: 'Imooc 首页',
-            movies: movies
+    Category
+        .find({})
+        .populate({
+            path: "movies",
+            options: {
+                limit: 5
+            }
         })
-    })
+        .exec(function(err, categories) {
+            res.render('index', {
+                title: 'Imooc 首页',
+                categories: categories
+            })
+        })
 }
